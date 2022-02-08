640f311833fa3176742a35072adecf9fbd90a89a
Abdeladem Fattah <dadem.fattah@gmail.com>
2022-02-08 21:35:27 +0100

    Revert "INIT COMMIT PROJECT 2021"
    This reverts commit 192b7fcc1a28095248a53a41e08cce602418db6b.

--- pendu_code/php/user.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_start();
-	if (isset($_SESSION["connection"]) && $_SESSION["connection"]==true){
-		echo $_SESSION["login"];
-	}
-?>
--- pendu_code/readme.txt
+++
@@ -1,3 +0,0 @@
-Utilisateur et mdp pour test
-utilisateur : boubia et mdp : 1234
-utilisateur : hmida et mdp : 1234
